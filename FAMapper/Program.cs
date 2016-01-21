d383a606e3f89eee6d147e18745684f92b981da4
Bipin Paul Bedi <bipinpaulbedi@gmail.com>
2016-01-21 10:13:11 +0530

    Initial version

---
+++ FAMapper/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Xml;
+using System.Linq;
+
+namespace FAMapper
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+
+            string[] feelingcollection = { "sorry", "apology" };
+            if (args.Length > 0)
+            {
+                Console.SetIn(new StreamReader(args[0]));
+            }
+
+            string line;
+            string xmlnodeString = string.Empty;           
+            bool datafound = false;
+            XmlDocument comment = new XmlDocument();
+
+            while ((line = Console.ReadLine()) != null)
+            {
+                if (line.StartsWith("<comments>"))
+                {
+                    datafound = true;
+                    continue;
+                }
+
+                if (datafound)
+                {
+                    xmlnodeString += line;
+                    if (xmlnodeString.EndsWith("/>"))
+                    {
+                        comment.LoadXml(xmlnodeString);
+                        if(feelingcollection.Any(x => comment.FirstChild.Attributes["Text"].Value.Contains(x)))                            
+                            Console.WriteLine(comment.FirstChild.Attributes["PostId"].Value);                        
+                        xmlnodeString = String.Empty;
+                    }
+                }
+                if(line.EndsWith("</comments>"))
+                    datafound = false;
+            }
+        }
+    }
+}
---
+++ FAReducer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace FAReducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string postId, lastPostId = null;
+            int count = 0;
+
+            if (args.Length > 0)
+            {
+                Console.SetIn(new StreamReader(args[0]));
+            }
+
+            while ((postId = Console.ReadLine()) != null)
+            {
+                if (postId != lastPostId)
+                {
+                    if (lastPostId != null)
+                        Console.WriteLine("{0}[{1}]", lastPostId, count);
+                    count = 1;
+                    lastPostId = postId;
+                }
+                else
+                    count += 1;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
